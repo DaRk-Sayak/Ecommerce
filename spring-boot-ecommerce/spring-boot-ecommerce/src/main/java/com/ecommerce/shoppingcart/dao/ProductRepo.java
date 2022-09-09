d870a6051623ad8ce2f30ce59a202573a13c9086
Sayak <sayaksarkar.official@gmail.com>
2022-09-09 20:09:10 +0530

    Added Angular UI

--- spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/ecommerce/shoppingcart/dao/ProductRepo.java
+++ spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/ecommerce/shoppingcart/dao/ProductRepo.java
@@ -2,6 +2,8 @@ package com.ecommerce.shoppingcart.dao;
 
 import com.ecommerce.shoppingcart.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 public interface ProductRepo extends JpaRepository<Product,Long> {
 }
